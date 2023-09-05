a88ccca5c8092f06f196c0a4a724eb80ab69c4b6
Matias Tite <matiastite1@gmail.com>
2023-09-04 21:49:20 -0500

    First commit adding the models from the main app of the project

---
+++ core/main/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+    project_manager = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.project_manager}"
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    members = models.ManyToManyField(User, through="GroupMembership")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.project}"
+
+
+class GroupMembership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} - {self.group}"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.group}"
