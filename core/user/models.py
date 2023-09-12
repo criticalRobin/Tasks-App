6b0c50ec68945cb6a7bdff8a6ffe277a7d2cec43
matite <matiastite1@gmail.com>
2023-09-12 15:57:49 -0500

    Commit adding my user model and making some changes on settings file

--- core/user/models.py
+++ core/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class MyUser(AbstractUser):
+    image = models.ImageField(upload_to="users/img", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
--- tasksapp/settings.py
+++ tasksapp/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+AUTH_USER_MODEL = "user.MyUser"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,7 +83,7 @@ WSGI_APPLICATION = "tasksapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "tasksappdb.sqlite3",
+        "NAME": BASE_DIR / "tasksApp.sqlite3",
     }
 }
 
